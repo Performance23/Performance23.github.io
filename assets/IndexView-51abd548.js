e49273308213fd985288a0c72848e154c64d96ee
bigdanhe <bigdanhe@gmail.com>
2023-08-19 23:53:28 +0100

    code lab

---
+++ assets/IndexView-51abd548.js
@@ -0,0 +1,119 @@
+import{o as n,c as o,d as u,t as d,n as m,e as h,f as p,g as f,_ as x,F as q,h as b,u as t,a as v,w as z,T as y,i as g}from"./index-4e853045.js";const C={__name:"MenuItem",props:{number:Number,title:String,isAcive:{type:Boolean,default:!1}},setup(i){return(e,r)=>(n(),o("div",{class:m(["bg-white w-full h-16 rounded-lg border flex items-center px-4 gap-4 mb-2 cursor-pointer",i.isAcive?"shadow-md":""])},[u("div",{class:m([i.isAcive?"bg-blue-500 shadow-lg":"","w-8 h-8 rounded-full bg-slate-500 flex items-center justify-center text-white text-lg font-medium"])},d(i.number),3),u("div",null,d(i.title),1)],2))}},V=h("course",()=>{const i=p({title:"Mastering Web Development with Vue.js and Tailwind CSS",lessons:[{title:"Introduction",number:1,content:`
+        <h2 class="text-3xl font-semibold mb-6">1. Approfondissement des Concepts Avancés</h2>
+        <h3 class="text-2xl font-semibold mt-8">Thème 1 : Utilisation Avancée de Composants Vue</h3>
+        <p>Nous allons découvrir comment tirer pleinement parti de la puissance des composants Vue.js. Voici quelques points clés :</p>
+        <ul class="list-disc pl-6 mt-4">
+            <li>Composants fonctionnels</li>
+            <li>Props avancés et validation</li>
+            <li>Communication entre composants</li>
+        </ul>
+    
+        <h3 class="text-2xl font-semibold mt-8">Thème 2 : Styles et Tailwind CSS</h3>
+        <p>Dans ce thème, nous explorerons comment appliquer des styles élégants à nos applications Vue.js en utilisant Tailwind CSS :</p>
+        <ol class="list-decimal pl-6 mt-4">
+            <li>Utilisation efficace des classes Tailwind</li>
+            <li>Personnalisation de thème</li>
+            <li>Responsive design avec classes utilitaires</li>
+        </ol>
+    
+        <h3 class="text-2xl font-semibold mt-8">Thème 3 : Gestion d'État Avancée</h3>
+        <p>Nous plongerons dans les méandres de la gestion d'état dans les applications Vue.js, en abordant :</p>
+        <ul class="list-disc pl-6 mt-4">
+            <li>Utilisation avancée de Vuex</li>
+            <li>Modules et organisation de l'état</li>
+            <li>Gestion des actions asynchrones</li>
+        </ul>
+    
+        <div class="bg-gray-100 p-6 mt-8">
+            <p class="italic">Astuce Pro : Prenez votre temps pour expérimenter ces concepts avancés dans des projets pratiques. Plus vous pratiquez, plus vous consoliderez votre compréhension.</p>
+        </div>
+    
+        <p class="mt-8">En fin de compte, cette leçon vous fournira des outils puissants pour construire des applications Vue.js sophistiquées et esthétiquement agréables. Continuez à vous engager avec le matériel et n'ayez pas peur d'explorer au-delà de ce qui est couvert ici.</p>
+        <p class="mt-8">En fin de compte, cette leçon vous fournira des outils puissants pour construire des applications Vue.js sophistiquées et esthétiquement agréables. Continuez à vous engager avec le matériel et n'ayez pas peur d'explorer au-delà de ce qui est couvert ici.</p>
+        <p class="mt-8">En fin de compte, cette leçon vous fournira des outils puissants pour construire des applications Vue.js sophistiquées et esthétiquement agréables. Continuez à vous engager avec le matériel et n'ayez pas peur d'explorer au-delà de ce qui est couvert ici.</p>
+        <p class="mt-8">En fin de compte, cette leçon vous fournira des outils puissants pour construire des applications Vue.js sophistiquées et esthétiquement agréables. Continuez à vous engager avec le matériel et n'ayez pas peur d'explorer au-delà de ce qui est couvert ici.</p>
+        <p class="mt-8">En fin de compte, cette leçon vous fournira des outils puissants pour construire des applications Vue.js sophistiquées et esthétiquement agréables. Continuez à vous engager avec le matériel et n'ayez pas peur d'explorer au-delà de ce qui est couvert ici.</p>
+        <p class="mt-8">En fin de compte, cette leçon vous fournira des outils puissants pour construire des applications Vue.js sophistiquées et esthétiquement agréables. Continuez à vous engager avec le matériel et n'ayez pas peur d'explorer au-delà de ce qui est couvert ici.</p>
+        <p class="mt-8">En fin de compte, cette leçon vous fournira des outils puissants pour construire des applications Vue.js sophistiquées et esthétiquement agréables. Continuez à vous engager avec le matériel et n'ayez pas peur d'explorer au-delà de ce qui est couvert ici.</p>
+        <p class="mt-8">En fin de compte, cette leçon vous fournira des outils puissants pour construire des applications Vue.js sophistiquées et esthétiquement agréables. Continuez à vous engager avec le matériel et n'ayez pas peur d'explorer au-delà de ce qui est couvert ici.</p>
+            `},{title:"Les bases",number:2,content:`
+        <h2 class="text-3xl font-semibold mb-6">2. Approfondissement des Concepts Avancés</h2>
+        <p class="text-base">Dans cette leçon, nous explorerons en détail les concepts avancés que vous avez précédemment abordés. Préparez-vous à une plongée profonde dans la complexité.</p>
+    
+        <h3 class="text-2xl font-semibold mt-8">Thème 1 : Utilisation Avancée de Composants Vue</h3>
+        <p>Nous allons découvrir comment tirer pleinement parti de la puissance des composants Vue.js. Voici quelques points clés :</p>
+        <ul class="list-disc pl-6 mt-4">
+            <li>Composants fonctionnels</li>
+            <li>Props avancés et validation</li>
+            <li>Communication entre composants</li>
+        </ul>
+    
+        <h3 class="text-2xl font-semibold mt-8">Thème 2 : Styles et Tailwind CSS</h3>
+        <p>Dans ce thème, nous explorerons comment appliquer des styles élégants à nos applications Vue.js en utilisant Tailwind CSS :</p>
+        <ol class="list-decimal pl-6 mt-4">
+            <li>Utilisation efficace des classes Tailwind</li>
+            <li>Personnalisation de thème</li>
+            <li>Responsive design avec classes utilitaires</li>
+        </ol>
+    
+        <h3 class="text-2xl font-semibold mt-8">Thème 3 : Gestion d'État Avancée</h3>
+        <p>Nous plongerons dans les méandres de la gestion d'état dans les applications Vue.js, en abordant :</p>
+        <ul class="list-disc pl-6 mt-4">
+            <li>Utilisation avancée de Vuex</li>
+            <li>Modules et organisation de l'état</li>
+            <li>Gestion des actions asynchrones</li>
+        </ul>
+    
+        <div class="bg-gray-100 p-6 mt-8">
+            <p class="italic">Astuce Pro : Prenez votre temps pour expérimenter ces concepts avancés dans des projets pratiques. Plus vous pratiquez, plus vous consoliderez votre compréhension.</p>
+        </div>
+    
+        <p class="mt-8">En fin de compte, cette leçon vous fournira des outils puissants pour construire des applications Vue.js sophistiquées et esthétiquement agréables. Continuez à vous engager avec le matériel et n'ayez pas peur d'explorer au-delà de ce qui est couvert ici.</p>
+        <p class="mt-8">En fin de compte, cette leçon vous fournira des outils puissants pour construire des applications Vue.js sophistiquées et esthétiquement agréables. Continuez à vous engager avec le matériel et n'ayez pas peur d'explorer au-delà de ce qui est couvert ici.</p>
+        <p class="mt-8">En fin de compte, cette leçon vous fournira des outils puissants pour construire des applications Vue.js sophistiquées et esthétiquement agréables. Continuez à vous engager avec le matériel et n'ayez pas peur d'explorer au-delà de ce qui est couvert ici.</p>
+        <p class="mt-8">En fin de compte, cette leçon vous fournira des outils puissants pour construire des applications Vue.js sophistiquées et esthétiquement agréables. Continuez à vous engager avec le matériel et n'ayez pas peur d'explorer au-delà de ce qui est couvert ici.</p>
+        <p class="mt-8">En fin de compte, cette leçon vous fournira des outils puissants pour construire des applications Vue.js sophistiquées et esthétiquement agréables. Continuez à vous engager avec le matériel et n'ayez pas peur d'explorer au-delà de ce qui est couvert ici.</p>
+        <p class="mt-8">En fin de compte, cette leçon vous fournira des outils puissants pour construire des applications Vue.js sophistiquées et esthétiquement agréables. Continuez à vous engager avec le matériel et n'ayez pas peur d'explorer au-delà de ce qui est couvert ici.</p>
+        <p class="mt-8">En fin de compte, cette leçon vous fournira des outils puissants pour construire des applications Vue.js sophistiquées et esthétiquement agréables. Continuez à vous engager avec le matériel et n'ayez pas peur d'explorer au-delà de ce qui est couvert ici.</p>
+        <p class="mt-8">En fin de compte, cette leçon vous fournira des outils puissants pour construire des applications Vue.js sophistiquées et esthétiquement agréables. Continuez à vous engager avec le matériel et n'ayez pas peur d'explorer au-delà de ce qui est couvert ici.</p>
+            `},{title:"Avancé",number:3,content:`
+        <h2 class="text-3xl font-semibold mb-6">3. Approfondissement des Concepts Avancés</h2>
+        <p class="text-base">Dans cette leçon, nous explorerons en détail les concepts avancés que vous avez précédemment abordés. Préparez-vous à une plongée profonde dans la complexité.</p>
+    
+        <h3 class="text-2xl font-semibold mt-8">Thème 1 : Utilisation Avancée de Composants Vue</h3>
+        <p>Nous allons découvrir comment tirer pleinement parti de la puissance des composants Vue.js. Voici quelques points clés :</p>
+        <ul class="list-disc pl-6 mt-4">
+            <li>Composants fonctionnels</li>
+            <li>Props avancés et validation</li>
+            <li>Communication entre composants</li>
+        </ul>
+    
+        <h3 class="text-2xl font-semibold mt-8">Thème 2 : Styles et Tailwind CSS</h3>
+        <p>Dans ce thème, nous explorerons comment appliquer des styles élégants à nos applications Vue.js en utilisant Tailwind CSS :</p>
+        <ol class="list-decimal pl-6 mt-4">
+            <li>Utilisation efficace des classes Tailwind</li>
+            <li>Personnalisation de thème</li>
+            <li>Responsive design avec classes utilitaires</li>
+        </ol>
+    
+        <h3 class="text-2xl font-semibold mt-8">Thème 3 : Gestion d'État Avancée</h3>
+        <p>Nous plongerons dans les méandres de la gestion d'état dans les applications Vue.js, en abordant :</p>
+        <ul class="list-disc pl-6 mt-4">
+            <li>Utilisation avancée de Vuex</li>
+            <li>Modules et organisation de l'état</li>
+            <li>Gestion des actions asynchrones</li>
+        </ul>
+    
+        <div class="bg-gray-100 p-6 mt-8">
+            <p class="italic">Astuce Pro : Prenez votre temps pour expérimenter ces concepts avancés dans des projets pratiques. Plus vous pratiquez, plus vous consoliderez votre compréhension.</p>
+        </div>
+    
+        <p class="mt-8">En fin de compte, cette leçon vous fournira des outils puissants pour construire des applications Vue.js sophistiquées et esthétiquement agréables. Continuez à vous engager avec le matériel et n'ayez pas peur d'explorer au-delà de ce qui est couvert ici.</p>
+        <p class="mt-8">En fin de compte, cette leçon vous fournira des outils puissants pour construire des applications Vue.js sophistiquées et esthétiquement agréables. Continuez à vous engager avec le matériel et n'ayez pas peur d'explorer au-delà de ce qui est couvert ici.</p>
+        <p class="mt-8">En fin de compte, cette leçon vous fournira des outils puissants pour construire des applications Vue.js sophistiquées et esthétiquement agréables. Continuez à vous engager avec le matériel et n'ayez pas peur d'explorer au-delà de ce qui est couvert ici.</p>
+        <p class="mt-8">En fin de compte, cette leçon vous fournira des outils puissants pour construire des applications Vue.js sophistiquées et esthétiquement agréables. Continuez à vous engager avec le matériel et n'ayez pas peur d'explorer au-delà de ce qui est couvert ici.</p>
+        <p class="mt-8">En fin de compte, cette leçon vous fournira des outils puissants pour construire des applications Vue.js sophistiquées et esthétiquement agréables. Continuez à vous engager avec le matériel et n'ayez pas peur d'explorer au-delà de ce qui est couvert ici.</p>
+        <p class="mt-8">En fin de compte, cette leçon vous fournira des outils puissants pour construire des applications Vue.js sophistiquées et esthétiquement agréables. Continuez à vous engager avec le matériel et n'ayez pas peur d'explorer au-delà de ce qui est couvert ici.</p>
+        <p class="mt-8">En fin de compte, cette leçon vous fournira des outils puissants pour construire des applications Vue.js sophistiquées et esthétiquement agréables. Continuez à vous engager avec le matériel et n'ayez pas peur d'explorer au-delà de ce qui est couvert ici.</p>
+        <p class="mt-8">En fin de compte, cette leçon vous fournira des outils puissants pour construire des applications Vue.js sophistiquées et esthétiquement agréables. Continuez à vous engager avec le matériel et n'ayez pas peur d'explorer au-delà de ce qui est couvert ici.</p>
+            `}]}),e=p(parseInt(localStorage.getItem("currentLessonIndex"))||0);f(e,l=>{localStorage.setItem("currentLessonIndex",l.toString())});const r=p(i.value.lessons[e.value]),a=()=>{e.value<i.value.lessons.length-1&&s(e.value+1)},c=()=>{e.value>0&&s(e.value-1)},s=l=>{e.value=l,r.value=i.value.lessons[l]};return{course:i,currentLesson:r,currentLessonIndex:e,changeLesson:s,next:a,prev:c}});const j={class:"h-screen w-full bg-blue-50 overflow-y-auto grid grid-cols-12 gap-5"},w={class:"bg-blue-50 col-span-3 overflow-y-auto fixed bottom-0 left-0 top-0 w-80 flex flex-col px-4 py-6 z-[500]"},_={class:"w-full"},S=["innerHTML"],E={class:"fixed bottom-20 left-80 right-0 grid grid-cols-12 py-5 pl-16 z-[1000]"},T={__name:"IndexView",setup(i){const e=V();return(r,a)=>{var c;return n(),o("div",j,[u("div",w,[u("ul",_,[(n(!0),o(q,null,b(t(e).course.lessons,(s,l)=>(n(),o("li",{class:"w-full",key:s.number},[v(C,{title:s.title,number:s.number,isAcive:t(e).currentLesson.number==s.number,onClick:A=>t(e).changeLesson(l)},null,8,["title","number","isAcive","onClick"])]))),128))])]),v(y,{name:"slide",mode:"out-in"},{default:z(()=>[(n(),o("div",{key:t(e).currentLessonIndex,class:"border shadow-lg rounded-lg bg-white col-span-6 p-5 text-justify col-start-5 my-5",innerHTML:t(e).currentLesson.content},null,8,S))]),_:1}),u("div",E,[t(e).currentLessonIndex>0?(n(),o("button",{key:0,class:"w-32 col-sapn-1 col-start-1 bg-white text-blue-500 py-2 px-5 shadow-md rounded",onClick:a[0]||(a[0]=(...s)=>t(e).prev&&t(e).prev(...s))}," Retour ")):g("",!0),t(e).currentLessonIndex<((c=t(e).course)==null?void 0:c.lessons.length)-1?(n(),o("button",{key:1,class:"w-32 bg-blue-500 text-white px-5 py-2 rounded shadow-md col-span-1 col-start-11",onClick:a[1]||(a[1]=(...s)=>t(e).next&&t(e).next(...s))}," Suivant ")):g("",!0)])])}}},I=x(T,[["__scopeId","data-v-13db8aad"]]);export{I as default};
